5d8abc7c58b97b2c784e116da09746d77ae44926
Yevhen Lehkiy <lehkiy.yevhen@steelkiwi.com>
2021-07-07 08:25:30 +0300

    update: remove useless endpoints

--- back/main.py
+++ back/main.py
@@ -3,20 +3,14 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from ranobes import Ranobes
+from ranobes import download_book
 
 app = FastAPI()
 
 
-class BookLink(BaseModel):
+class BookInput(BaseModel):
     link: str
     chapters_count: Optional[int] = 1
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
 
 
 @app.get("/")
@@ -24,21 +18,10 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/get_some/{item_id}")
-def get_some(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Optional[str] = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
-
-
 @app.post("/download/")
-async def update_item(book: BookLink):
-    ranobe = Ranobes(book.link)
-    # b = ranobe.get_book()
+async def update_item(book_input: BookInput):
+    # ranobe = Ranobes(book.link)
+    # chapters = await download_chapters(ranobe.chapters_links)
 
-    return "aaa"  # b.txt
+    book = await download_book(book_input.link)
+    return book.txt
